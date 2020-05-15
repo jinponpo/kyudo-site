086a28104575a3114325813db432bc0aeb234940
EC2 Default User <ec2-user@ip-172-31-40-62.ap-northeast-1.compute.internal>
2020-05-15 10:12:27 +0000

    コピーライト変更

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer bg-light" style="opacity: 0.7;">
-    <small class='copyright  text-dark'>FreeGame 2020 copyright</small>
+    <small class='copyright  text-dark'>Laravel JINTODO 2020 copyright</small>
 </footer>
