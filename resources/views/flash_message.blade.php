6829fdc436d5599b6ba94d169956c21646e795dd
EC2 Default User <ec2-user@ip-172-31-40-62.ap-northeast-1.compute.internal>
2020-05-18 06:35:55 +0000

    フラッシュメッセージ修正

--- resources/views/flash_message.blade.php
+++ resources/views/flash_message.blade.php
@@ -1,5 +1,5 @@
 @if (session('flash_message'))
-    <div class="flash_message bg-success text-center py-3 my-0 mt100">
+    <div class="flash_message bg-success text-center py-3 my-0">
         {{ session('flash_message') }}
     </div>
 @endif
