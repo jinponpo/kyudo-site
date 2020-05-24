0e1aff9a6afb0b91cdce155bc87a5154e87502d2
github jinponpo <jk.kunia@gmail.com>
2020-05-24 19:05:33 +0900

    タグ入力時のスペースキーを確定キーに追加

--- public/js/app.js
+++ public/js/app.js
@@ -2074,6 +2074,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   components: {
@@ -39140,7 +39141,8 @@ var render = function() {
         attrs: {
           tags: _vm.tags,
           placeholder: "タグを5個まで入力できます",
-          "autocomplete-items": _vm.filteredItems
+          "autocomplete-items": _vm.filteredItems,
+          "add-on-key": [13, 32]
         },
         on: {
           "tags-changed": function(newTags) {
