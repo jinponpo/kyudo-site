19420525f796cda1087f0e8ac082d60b4db605b4
github jinponpo <jk.kunia@gmail.com>
2020-05-25 22:02:24 +0900

    マイページ修正

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -1,33 +1,45 @@
-<div class="card mt-3">
-      <div class="card-body">
-        <div class="d-flex flex-row">
-          <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
-            <i class="fas fa-user-circle fa-3x"></i>
-          </a>
-          @if( Auth::id() !== $user->id )
-            <follow-button
-              class="ml-auto"
-              :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))'
-              :authorized='@json(Auth::check())'
-              endpoint="{{ route('users.follow', ['name' => $user->name]) }}"
-            >
-            </follow-button>
-          @endif
-        </div>
-        <h2 class="h5 card-title m-0">
+<div class="mt40 mb40">
+  <div class="row">
+    <div class="col-md-4 text-center">
+        @if ($user->profile_photo)
+          <p>
+            <img class="round-img" src="{{ asset('storage/user_images/' . $user->profile_photo) }}"/>
+          </p>
+          @else
+            <img class="round-img" src="{{ asset('/images/blank_profile.png') }}"/>
+        @endif
+      </div>
+      <div class="col-md-8">
+      <div class="row">
+        <h1>
           <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
             {{ $user->name }}
           </a>
-        </h2>
+        </h1>
+
+        @if( Auth::id() == $user->id )
+        <a class="btn btn-outline-dark common-btn ml-auto mr-auto" href="/users/edit">プロフィールを編集</a>
+        @endif
+
+        @if( Auth::id() !== $user->id )
+          <follow-button
+            class="ml-auto"
+            :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))'
+            :authorized='@json(Auth::check())'
+            endpoint="{{ route('users.follow', ['name' => $user->name]) }}"
+          >
+          </follow-button>
+        @endif
       </div>
-      <div class="card-body">
-        <div class="card-text">
-        <a href="{{ route('users.followings', ['name' => $user->name]) }}" class="text-muted">
+
+      <div class="row">
+          <a href="{{ route('users.followings', ['name' => $user->name]) }}" class="text-muted h4 mt20 mr20">
             {{ $user->count_followings }} フォロー
           </a>
-          <a href="{{ route('users.followers', ['name' => $user->name]) }}" class="text-muted">
+          <a href="{{ route('users.followers', ['name' => $user->name]) }}" class="text-muted h4 mt20">
             {{ $user->count_followers }} フォロワー
           </a>
         </div>
       </div>
     </div>
+  </div>
