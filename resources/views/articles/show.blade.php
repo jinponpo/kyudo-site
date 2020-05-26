062002b5e1c2ecc0c56b28a245fb86a70e9cd8ad
github jinponpo <jk.kunia@gmail.com>
2020-05-26 13:46:31 +0900

    投稿詳細ページ　タイトル付け

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
+<div class="container">
+  <div class="text-align-center h1 mt10 mb20">
+    〜投稿詳細ページ〜
+  </div>
     @include('card')
     <iframe id='map' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBQCqrHT55gPe0VRd0x_WGbXl7PWFmObEE&amp;q={{ $article->body }}'
       class='mt60' width='100%' height='480' frameborder='0'>
